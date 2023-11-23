80ea6f1c84985986cc30993d25028ea891e078e7
Rick Steawart <rickstewartdev@gmail.com>
2023-11-23 17:06:43 +0000

    exercise end

--- scripts/win2.js
+++ scripts/win2.js
@@ -1,17 +1,19 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractName = "Game1";
+const gameAddr = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const contractName = "Game2";
 
 async function main() {
   // attach to the game
   const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
   // do whatever you need to do to win the game here:
-  const tx = await game.win();
+  const tx = await game.setX(20);
+  const tx2 = await game.setY(30);
+  const tx3 = await game.win();
 
   // did you win? Check the transaction receipt!
   // if you did, it will be in both the logs and events array
-  const receipt = await tx.wait();
+  const receipt = await tx3.wait();
   console.log(receipt);
 }
 
--- scripts/win5.js
+++ scripts/win5.js
@@ -1,17 +1,19 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractName = "Game1";
+const gameAddr = "0x0B306BF915C4d645ff596e518fAf3F9669b97016";
+const contractName = "Game5";
 
 async function main() {
   // attach to the game
   const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
   // do whatever you need to do to win the game here:
-  const tx = await game.win();
+  const tx = await game.giveMeAllowance(10000);
+  const tx2 = await game.mint(10000);
+  const tx3 = await game.win();
 
   // did you win? Check the transaction receipt!
   // if you did, it will be in both the logs and events array
-  const receipt = await tx.wait();
+  const receipt = await tx3.wait();
   console.log(receipt);
 }
 
